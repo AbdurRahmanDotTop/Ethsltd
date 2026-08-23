1148f4a2a5cbffbb5ac099bda0f91298fcebb760
AbdurRahmanDotTop <abdurrahmandottopofficial@gmail.com>
2026-08-23 20:34:01 +0530

    Fix push.bat for SSH paths

--- push.bat
+++ push.bat
@@ -1,2 +1,2 @@
-set GIT_SSH_COMMAND=C:\Windows\System32\OpenSSH\ssh.exe -i "C:\Users\abdur\.ssh\id_ed25519" -o StrictHostKeyChecking=accept-new
+set GIT_SSH_COMMAND=C:/Windows/System32/OpenSSH/ssh.exe -i "C:/Users/abdur/.ssh/id_ed25519" -o StrictHostKeyChecking=accept-new
 git push -u origin main
